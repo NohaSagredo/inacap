8cc102b72ee2002a278f8a7986686afba734495a
NohaSagredo <r.p.sagredo@gmail.com>
2025-05-13 10:35:20 -0400

    Ejemplo de usar librerias y debug

--- Introducción a la programación segura/Ejemplo1.py
+++ Introducción a la programación segura/Ejemplo1.py
@@ -3,3 +3,7 @@ import math
 radio = float(input("Ingrese el radio de la circunferencia: "))
 area = math.pi * radio ** 2
 print(area)
+
+raiz_cuadrada = math.sqrt(radio)
+print()
+print(raiz_cuadrada)
